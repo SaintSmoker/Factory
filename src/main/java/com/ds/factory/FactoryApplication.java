c0b18ca879b285c51d75536debcfb2f63243743f
SaintSmoker <1273067731@qq.com>
2020-07-05 22:43:59 +0800

    划划划

--- src/main/java/com/ds/factory/FactoryApplication.java
+++ src/main/java/com/ds/factory/FactoryApplication.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.ds.factory.datasource.mappers")
 public class FactoryApplication {
         //管理员测试用户名：admin；密码：123456
+    //pull测试
     public static void main(String[] args) {
         SpringApplication.run(FactoryApplication.class, args);
     }
